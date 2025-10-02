b5bcd712695e7b9580734daf7c4179fa9bf95a02
Precious Samuel <koja7097@gmail.com>
2025-10-02 15:03:33 +0100

    simple authentication update on the dashboard section and some updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,6 +14,27 @@ import BookingSection from "@/components/BookingSection";
 
 export default function DashboardPage() {
 
+    // Authentication
+    const [isUnlocked, setIsUnlocked] = useState(false)
+     const [loginInput, setLoginInput] = useState("")
+      const [error, setError] = useState("")
+
+      // settings: Allowed Pin or Name
+      const allowedPin = "123456"
+      const allowedName = "Club Owner"
+
+      const handleLogin = () => {
+        if (loginInput === allowedPin || loginInput.toLowerCase() === allowedName.toLowerCase()) {
+            setIsUnlocked(true)
+            setError("")
+        } else {
+            setError("Access Denied. Wrong Pin or Name.")
+        }
+      } 
+
+
+    //States & Management
+
     const [activeTap, setActiveTap] = useState("events")
 
 const [events, setEvents] = useState<EventItem[]>(mockEvents)
@@ -83,11 +104,33 @@ const addPost = () => {
         <>
          <Navigation/>
     <div className="flex min-h-screen">
-      {/* sideBar */}
-      <aside className="flex flex-col fixed inset-y-0 left-0 bg-black w-16 md:w-64 transition-all duration-300">
+
+    {/* Login */}
+    {!isUnlocked && (
+    <div className="fixed inset-0 bg-black bg-opacity-60 flex item-center justify-center z-50">
+    <div className="bg-gray p-8 rounded-xl shadow-lg w-96 text-center">
+    <h2 className="text-xl font-semibold mb-4">Enter Pin or Name</h2>
+    <input type="text"
+    placeholder="Enter Pin or Name"
+    value={loginInput}
+    onChange={(e)=> setLoginInput(e.target.value)}
+    className="border w-full p-2 rounded mb-4"
+    />
+    {error && <p className="text-red-600 text-sm mb-2">{error}</p>}
+    <button
+    onClick={handleLogin}
+    className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 w-full"
+    >Unlock</button>
+    </div>
+    </div>
+    )}
+
+
+    
+      <aside className="flex flex-col fixed inset-y-0 left-0 bg-black w-18 md:w-44 transition-all duration-300">
       <SideSection activeTap={activeTap} setActiveTap={setActiveTap}/>
       </aside>
-
+          {isUnlocked && (
           <main className="flex-1 p-6 space-y-10 overflow-y-auto ml-16 md:ml-64 w-full">
          <div className="flex items-center justify-between p-3 shadow sticky top-0 z-30 md:hidden">
               <h1 className="text-2xl font-bold text-center">Club Owners Dashboard</h1>
@@ -119,7 +162,9 @@ const addPost = () => {
      )}
 
         </main>
+        )}
     </div>
+    
     </>
     )
 }
--- components/SideSection.tsx
+++ components/SideSection.tsx
@@ -9,19 +9,19 @@ type SideSectionProps = {
 }
 export default function SideSection({activeTap, setActiveTap}: SideSectionProps) {
     const tabs = [
-        {id: "events", label: "Events", icon: <Calendar size={20}/>},
-        {id: "feed", label: "Feed", icon: <Newspaper size={20}/>},
-        {id: "booking", label: "Booking", icon: <ListCheck size={20}/>},
+        {id: "events", label: "Events", icon: <Calendar size={22}/>},
+        {id: "feed", label: "Feed", icon: <Newspaper size={22}/>},
+        {id: "booking", label: "Booking", icon: <ListCheck size={22}/>},
     ]
     return (
         <aside className="w-64 flex-col bg-white-900 text-dark p-6 space-y-4">
             <h2 className="text-xl font-bold mb-6 ">Dashbaord Menu</h2>
-            <nav className="space-y-4 flex flex-col  h-full hover:bg-blue-600 ">
+            <nav className="space-y-4 flex flex-col  h-full ">
                 {tabs.map((tab)=> (
                     <button
                     key={tab.id}
                     onClick={()=> setActiveTap(tab.id)}
-                    className={`flex md:flex-row flex-col item-center md:item-start md:sppace-x-2 p-1   hover:bg-blue-600  ${activeTap} `}
+                    className={`flex md:flex-row flex-col item-center md:item-start md:sppace-x-2 p-1 hover:color-blue-600  ${activeTap} `}
                     >
                    {/* icon visibility */}  
                   <div className="flex-shrink-0"> {tab.icon}  </div>
